b71e926d5c7eff13792099e7b7b1885e64a9e044
Paul Laney <paul@monkii.com>
2018-06-07 21:42:47 +1000

    Added Carbon Str Collection + helpers

---
+++ src/Support/Carbon.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Monkii\Support;
+
+use Carbon\Carbon as BaseCarbon;
+
+class Carbon extends BaseCarbon
+{
+}
---
+++ src/Support/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Monkii\Support;
+
+use Tightenco\Collect\Support\Collection as BaseCollection;
+
+class Collection extends BaseCollection
+{
+}
